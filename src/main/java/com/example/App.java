69dd6c5218a0d66dd121e21ed274bbbef1b22e38
sgdesai74 <sgdesai74@gmail.com>
2026-03-05 12:42:55 -0500

    updated
    updated

---
+++ src/main/java/com/example/App.java
@@ -0,0 +1,11 @@
+package com.example.app;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello from Maven + Jenkins!");
+    }
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
